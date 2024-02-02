51f5cbf7000c00092a35121f2fa2ab99328b03ac
marim27 <marimmohmed@outlook.com>
2024-02-02 23:52:29 +0200

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import NotFound from "./components/NotFound";
+import NotFound from "./pages/NotFound";
 import DefaultLayout from "./layout/DefaultLayout";
 import Home from "./pages/Home";
 import Register from "./pages/register";
 import Layout from "./layout/Layout";
+import HandelErorr from "./pages/handelErorr";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
     children: [
-      {index: true, element: <Home />},
+      {index: true, element: <Home />, errorElement:<HandelErorr/>},
       {path:"*", element: <NotFound/>}
     ]
   },
   {
     element: <Layout />,
     children: [
-      { path: "/register", element: <Register /> },
+      { path: "/register", element: <Register />, errorElement:<HandelErorr/> },
     ],
   },
 ]);
