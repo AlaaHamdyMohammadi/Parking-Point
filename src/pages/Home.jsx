b31c848489465ccdecb1c028b8dd27f6d314fec7
marim27 <marimmohmed@outlook.com>
2024-02-07 23:38:53 +0200

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import HomeLogout from "../components/home/HomeLogout";
 import HomeLogin from "./../components/driver/HomeLogin";
-import ParkingHome from "./parking/ParkingHome";
 
 export default function Home() {
   const [auth, setAuth] = useState(false);
   return (
     <>
-      {auth && <HomeLogin />}
+      <HomeLogin />
       {auth && <HomeLogout />}
-      <ParkingHome />
     </>
   );
 }
