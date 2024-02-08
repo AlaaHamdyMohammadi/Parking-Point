7c10ecb40c7b91f499df3ad14cd489aaf172788b
marim27 <marimmohmed@outlook.com>
2024-02-08 15:08:01 +0200

    ..

--- src/pages/parking/AddParking.jsx
+++ src/pages/parking/AddParking.jsx
@@ -41,12 +41,6 @@ export default function AddParking() {
         <div className={`p-5`}>
           <h5 className={`text-secondary text-center`}>يمكن إضافة ثلاث صور فقط</h5>
           <div className={` p-2 d-flex justify-content-center`}>
-          {imgArr.length < 3 && (
-            <div className={`col-4 mx-2 border d-flex p-5 d-flex align-items-center justify-content-center`} role="button" onClick={clickImgInput}>
-              <MdOutlineAddBusiness className={`fs-1 `} />
-              <input type="file" name="parkingImage" multiple id="images" accept="image/*" hidden ref={profileImgRef} onChange={(e) => saveImageArr(e)} />
-            </div>
-             )}
              {imgArr.map((image, index) => (
              <div className={`col-4 mx-2 border d-flex d-flex align-items-center justify-content-center position-relative`} key={index}>
                <div onClick={() => removeImage(index)} className={`position-absolute top-0 end-0`} role="button">
@@ -56,6 +50,12 @@ export default function AddParking() {
             </div>
             
             ))}
+ {imgArr.length < 3 && (
+            <div className={`col-4 mx-2 border d-flex p-5 d-flex align-items-center justify-content-center`} role="button" onClick={clickImgInput}>
+              <MdOutlineAddBusiness className={`fs-1 `} />
+              <input type="file" name="parkingImage" multiple id="images" accept="image/*" hidden ref={profileImgRef} onChange={(e) => saveImageArr(e)} />
+            </div>
+             )}
           </div>
         </div>
       </div>
