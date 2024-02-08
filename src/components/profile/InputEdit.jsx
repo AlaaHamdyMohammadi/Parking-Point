f56206ae6a08e97d7d52e7fc08ea61e21f7f8afc
marim27 <marimmohmed@outlook.com>
2024-02-08 15:02:43 +0200

    add parkıng

--- src/components/profile/InputEdit.jsx
+++ src/components/profile/InputEdit.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function InputEdit({ label, placeholder, type ,setState }) {
   const inputChang = (event) => {
     setState(event.target.value)
--- src/pages/parking/AddParking.jsx
+++ src/pages/parking/AddParking.jsx
@@ -1,6 +1,64 @@
+import { useRef, useState } from "react";
+import { MdOutlineAddBusiness } from "react-icons/md";
+import { MdClose } from "react-icons/md";
 
 export default function AddParking() {
+  const profileImgRef = useRef(null);
+  function clickImgInput() {
+      profileImgRef.current.click()
+  }
+  const [imgArr, setImgArr] = useState([]);
+  function saveImageArr(eve) {
+    setImgArr((i) => [...i, ...Array.from(eve.target.files)]);
+    // setProduct({ ...product, images: [...imgArr, ...Array.from(eve.target.files)] });
+  }
+  function showImages(image) {
+    try {
+      return URL.createObjectURL(image);
+    } catch (err) {
+      // return `${axiosInstanceProducts.defaults.baseURL}/${image}`;
+    }
+  }
+
+  const removeImage = (index) => {
+    setImgArr((prevImgArr) => {
+      const updatedImgArr = [...prevImgArr];
+      updatedImgArr.splice(index, 1);
+      // setProduct({
+      //   ...product,
+      //   images: [...updatedImgArr],
+      // });
+
+      return updatedImgArr;
+    });
+    // console.log(imgArr);
+  };
+
   return (
-    <div>AddParking</div>
+    <>
+      <h3>لإضافة موقف يرجي ادخال البيانات الصحيحة</h3>
+      <div className={`card w-75 p-2`}>
+        <div className={`p-5`}>
+          <h5 className={`text-secondary text-center`}>يمكن إضافة ثلاث صور فقط</h5>
+          <div className={` p-2 d-flex justify-content-center`}>
+          {imgArr.length < 3 && (
+            <div className={`col-4 mx-2 border d-flex p-5 d-flex align-items-center justify-content-center`} role="button" onClick={clickImgInput}>
+              <MdOutlineAddBusiness className={`fs-1 `} />
+              <input type="file" name="parkingImage" multiple id="images" accept="image/*" hidden ref={profileImgRef} onChange={(e) => saveImageArr(e)} />
+            </div>
+             )}
+             {imgArr.map((image, index) => (
+             <div className={`col-4 mx-2 border d-flex d-flex align-items-center justify-content-center position-relative`} key={index}>
+               <div onClick={() => removeImage(index)} className={`position-absolute top-0 end-0`} role="button">
+                  <MdClose className="fs-3 bgColor text-white" />
+              </div>
+              <img className="w-100" src={showImages(image)} alt="Selected" />
+            </div>
+            
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
