ca1ef433fc703e924f45de75eec404a31146a557
Esraa Fathy <esraafa2411@gmail.com>
2024-02-09 13:49:58 -0800

    edit

--- src/pages/parking/ParkingHome.jsx
+++ src/pages/parking/ParkingHome.jsx
@@ -1,20 +1,19 @@
-import ParkingCard from '../../components/dashboader/ParkingCard';
-import ParkingFilter from '../../components/dashboader/ParkingFilter';
+import ParkingCard from "../../components/dashboader/ParkingCard";
+import ParkingFilter from "../../components/dashboader/ParkingFilter";
 export default function ParkingHome() {
-
   return (
     <div>
       <div></div>
       <div className={`d-flex`}>
-        <ParkingFilter value={'all'} text='الجميع' count={1} />
-        <ParkingFilter value={'active'} text='نشط' count={6} />
-        <ParkingFilter value={'pending'} text='قيد الانتظار' count={2} />
-        <ParkingFilter value={'inactive'} text='غير نشط' count={3} />
+        <ParkingFilter value={"all"} text="الجميع" count={1} />
+        <ParkingFilter value={"active"} text="نشط" count={6} />
+        <ParkingFilter value={"pending"} text="قيد الانتظار" count={2} />
+        <ParkingFilter value={"inactive"} text="غير نشط" count={3} />
       </div>
-      <ParkingCard/>
-      <ParkingCard/>
-      <ParkingCard/>
-      <ParkingCard/>
+      <ParkingCard />
+      {/* <ParkingCard/> */}
+      {/* <ParkingCard/> */}
+      {/* <ParkingCard/> */}
     </div>
-  )
+  );
 }
