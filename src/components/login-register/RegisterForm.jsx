12981c93f5501857a93d30c488628f1498479669
marim27 <marimmohmed@outlook.com>
2024-02-14 17:36:10 +0200

    login page

--- src/components/login-register/RegisterForm.jsx
+++ src/components/login-register/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import classes from "./../../styles/register.module.css";
+import classes from "./../../styles/formStyles.module.css";
 export default function RegisterForm() {
     const [fristName, setFristName] = useState("");
     const [lastName, setILastName] = useState("");
@@ -28,50 +28,50 @@ export default function RegisterForm() {
     };
     return (
         <>
-            <div className="fs-4 fw-semibold">
-                <form action=""></form>
-                <div className={`d-flex gap-4`}>
-                    <div className="d-flex  flex-column">
-                        <label htmlFor="fullname">الأسم الاول </label>
-                        <input type="text" className={`${classes.inputcolor}`} id="fullname" />
+                <form action="" className="my-5">
+            <div className="fs-4">
+                <div className={`row`}>
+                    <div className="col-md-5 col-12 ms-md-5 ">
+                        <label className="fs-5" htmlFor="fullname">الأسم الاول </label>
+                        <input type="text" className={`${classes.input} form-control border-secondary shadow-none`} id="fullname" />
                     </div>
-                    <div className="d-flex flex-column">
-                        <label htmlFor="lastlname">الأسم الاخير</label>
-                        <input className={`${classes.inputcolor}`} type="text" id="lastname" />
+                    <div className="col-md-5 col-12 me-md-4">
+                        <label className="fs-5" htmlFor="lastlname">الأسم الاخير</label>
+                        <input className={`${classes.input} form-control border-secondary shadow-none`} type="text" id="lastname" />
                     </div>
                 </div>
-                <div>
-                    <label htmlFor="email">الايميل</label>
-                    <input type="email" id="email" />
+                <div className="mt-4">
+                    <label className="fs-5" htmlFor="email">الايميل</label>
+                    <input type="email" id="email" className={`${classes.input} form-control border-secondary shadow-none`}/>
                 </div>
-                <div>
-                    <label htmlFor="password">كلمة السر</label>
-                    <input type="password" id="password" />
+                <div className="mt-4">
+                    <label className="fs-5" htmlFor="password">كلمة السر</label>
+                    <input type="password" id="password" className={`${classes.input} form-control border-secondary shadow-none`}/>
                 </div>
-                <div>
-                    <label htmlFor="confirmpassword">تأكيد كلمه السر</label>
-                    <input type="password" id="confirmpassword" />
+                <div className="mt-4">
+                    <label className="fs-5" htmlFor="confirmpassword">تأكيد كلمه السر</label>
+                    <input type="password" id="confirmpassword" className={`${classes.input} form-control border-secondary shadow-none`}/>
                 </div>
-                <div>
-                    <label htmlFor="nummob">رقم الهاتف </label>
-                    <input type="number" id="nummob" />
+                <div className="mt-4">
+                    <label className="fs-5" htmlFor="nummob">رقم الهاتف </label>
+                    <input type="text" id="nummob" className={`${classes.input} form-control border-secondary shadow-none`}/>
                 </div>
-                <div>
-                <label> نوع الحساب</label>
-                </div>
+                <label className="mt-4 fs-5 col-md-4 col-12"> نوع الحساب</label>
+                <div className="mt-2 row">
+                <div className="col-md-6 col-12 mt-3 mt-md-0">
                 <input
                     type="radio"
                     name="acctype"
                     id="driver"
                     value="driver"
-                    onChange={(eve) => {
-                        displayIsDriver(eve);
-                    }}
-                    className={`${classes.inputFilter}`}
-                />
-                <label className={`m-2 `} htmlFor="driver">
+                    onChange={(eve) => { displayIsDriver(eve);}}
+                    className={`${classes.inputFilter} ms-2`}
+                    />
+                <label className="fs-5 ms-md-1" htmlFor="driver">
                     سائق
                 </label>
+                </div>
+                <div className="col-md-6 col-12 mt-3 mt-md-0">
                 <input
                     type="radio"
                     id="owner"
@@ -80,40 +80,46 @@ export default function RegisterForm() {
                     onChange={(eve) => {
                         displayIsOwner(eve);
                     }}
-                    className={` mt-4 ${classes.inputFilter}`}
-                />
-                <label className={`m-2 `} htmlFor="owner">
+                    className={`ms-1 ${classes.inputFilter}`}
+                    />
+                <label className="fs-5" htmlFor="owner">
                     صاحب موقف
                 </label>
+                </div>
+                    </div>
+
                 {isDriver && (
                     <>
-                        <div>
-                            <label htmlFor="numboard">رقم اللوحة</label>
-                            <input type="number" id="numboard" />
+                        <div className="mt-4">
+                            <label className="fs-5" htmlFor="numboard">رقم اللوحة</label>
+                            <input type="text" id="numboard" className={`${classes.input} form-control border-secondary shadow-none`}/>
                         </div>
-                        <div>
-                            <label htmlFor="cars">نوع المركبة</label>
-                            <select id="cars" name="cars">
+                        <div className="mt-4">
+                            <label className="fs-5" htmlFor="cars">المركبة</label>
+                            <select id="cars" name="cars" className={`${classes.input} form-control border border-secondary shadow-none`}>
+                                <option value="" hidden>اختر نوع المركبة</option>
                                 <option value="car">سيارة</option>
                             </select>
                         </div>
-                        <input type="submit" value="submit" className={`${classes.submit}`} />
+                        <input type="submit" value="submit" className={`${classes.formBtn} text-center bgColor text-white btn mt-5`} />
                     </>
                 )}
                 {isOwner && (
                     <>
-                        <div className={`d-flex justify-content-between mt-3`}>
-                            <div className={`d-flex col-5`}>
-                                <label className={` ms-2`} htmlFor="Governorate">المحافظه</label>
-                                <select className={`my-2 ms-2`} id="cars" name="cars">
+                        <div className={`row mt-3`}>
+                            <div className={`col-md-5 col-12`}>
+                                <label className="fs-5" htmlFor="Governorate">المحافظه</label>
+                                <select className={`${classes.input} form-control border border-secondary shadow-none`} id="cars" name="cars">
+                                    <option value="" hidden>اختر المحافظه</option>
                                     <option value="masqt">مسقط</option>
                                 </select>
                             </div>
-                            <div className={`d-flex col-5`}>
-                                <label className={`ms-2`} htmlFor="Governorate">
+                            <div className={`col-md-5 col-12`}>
+                                <label className="fs-5" htmlFor="Governorate">
                                     الولاية
                                 </label>
-                                <select className={`my-2`} id="cars" name="cars">
+                                <select id="cars" name="cars" className={`${classes.input} form-control border border-secondary shadow-none`}>
+                                    <option value="" hidden>اختر الولاية</option>
                                     <option value="masqt">مسقط</option>
                                     <option value="mtrh">مطرح</option>
                                     <option value="seeb">السيب</option>
@@ -123,16 +129,17 @@ export default function RegisterForm() {
                                 </select>
                             </div>
                         </div>
-                        <div>
-                        <label className={`mt-3`} htmlFor="area">
+                        <div className="mt-4">
+                        <label className="fs-5" htmlFor="area">
                             المنطقه{" "}
                         </label>
-                        <input type="text" id="area" />
+                        <input type="text" id="area" className={`${classes.input} form-control border-secondary shadow-none`}/>
                         </div>
-                        <input type="submit" value="submit" className={`${classes.submit}`} />
+                        <input type="submit" value="submit" className={`${classes.formBtn} text-center bgColor text-white btn mt-5`} />
                     </>
                 )}
             </div>
+            </form>
         </>
     );
 }
