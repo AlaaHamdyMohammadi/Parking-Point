73d8afa569ee2086192acd98ef2fb1abafcc5056
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-17 04:42:51 +0200

    Add Live Location

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,30 +1,58 @@
 // import React from "react";
-import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import { useEffect, useState } from "react";
+import ReactMapGL, { Marker, NavigationControl } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-// const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
-// const TOKEN ="pk.eyJ1IjoiYWxhYWhhbWR5MiIsImEiOiJjbHNvcmJsZ2kwaHFlMm1rNXJkMWYxZjhkIn0.JKB_JwB_XSgRR2OJsjd5eA";
+//const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 //mapStyle="mapbox://styles/alaahamdy2/clsoqyy67004801pk549pcyc4"
 function Map() {
-  const [viewPort, setViewPort] = useState({
-    latitude: 30.475425,
-    longitude: 31.1912051,
-    width: "100vw",
-    height: "100vw",
+  const [viewport, setViewport] = useState({
+    latitude: 0,
+    longitude: 0,
     zoom: 10,
-    
   });
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setViewport((prevViewport) => ({
+          ...prevViewport,
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }));
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  
+
+  
+
   return (
-    <div>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <ReactMapGL
-        {...viewPort}
+        {...viewport}
         mapboxAccessToken={
           "pk.eyJ1IjoiYWxhYWhhbWR5MiIsImEiOiJjbHNvcmJsZ2kwaHFlMm1rNXJkMWYxZjhkIn0.JKB_JwB_XSgRR2OJsjd5eA"
         }
-        mapStyle={"mapbox://styles/alaahamdy2/clsoqyy67004801pk549pcyc4"}
-        onViewportChange={(viewport) => setViewPort(viewport)}
+        width="100%"
+        height="100%"
+        transitionDuration="200"
+        mapStyle={"mapbox://styles/alaahamdy2/clsp701hd005a01pkhrmygybf"}
+        onViewportChange={(viewport) => setViewport(viewport)}
       >
+        <Marker
+          draggable
+          latitude={viewport.latitude}
+          longitude={viewport.longitude}
+          offsetLeft={-20}
+          offsetTop={-10}
+        />
         
+
+        <NavigationControl position="top-right" />
       </ReactMapGL>
     </div>
   );
