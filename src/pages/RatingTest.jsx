1b7c14139b3c5c4223e2753972762ec1944adc36
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-02-24 22:34:42 +0200

    Add Rating

--- src/pages/RatingTest.jsx
+++ src/pages/RatingTest.jsx
@@ -1,47 +1,50 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
 import axiosInstanceParking from "../axiosConfig/instanc";
 import StarRatingModal from "../components/driver/StarRatingModal";
+import { useSelector } from "react-redux";
 
 function RatingTest() {
-  const [rating, setRating] = useState(null);
-  const [hover, setHover] = useState(null);
-  const [averageRating, setAverageRating] = useState(null);
+  const [averageRating, setAverageRating] = useState([]);
+  const token = useSelector((state) => state.token.token);
 
-//   useEffect(() => {
-//     axiosInstanceParking.get("/parkings").then((res) => {
-//       console.log(res);
-//     });
-//   }, []);
+  useEffect(() => {
+    axiosInstanceParking.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${token}`;
+
+    axiosInstanceParking.get("/parkings").then((res) => {
+      console.log(res.data.doc);
+      setAverageRating(res.data.doc);
+    });
+  }, [token]);
 
   return (
     <>
-      {[...Array(5)].map((star, i) => {
-        const ratingValue = i + 1;
-        return (
-          <label key={i}>
-            <input
-              type="radio"
-              className="RadioNone"
-              name="rating"
-              value={ratingValue}
-              onClick={() => setRating(ratingValue)}
-            />
-
-            <FaStar
-              className="cursor-pointer pointer "
-              color={ratingValue <= (hover || rating) ? "#f1a525" : "#aaa5a5"}
-              size={12}
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(null)}
-            />
-          </label>
-        );
-      })}
-      <StarRatingModal/>
+      {averageRating.map((rating) => (
+        <RatingComponent key={rating._id} rating={rating.rate} />
+      ))}
+      <StarRatingModal />
     </>
   );
 }
 
 export default RatingTest;
+
+function RatingComponent({ rating }) {
+  const createArray = (length) => Array.from({ length }, (_, i) => i);
+
+  return (
+    <div>
+      {[...createArray(rating)].map((_, index) => (
+        <FaStar key={index} style={{ color: "#f1a525" }} />
+      ))}
+      {[...createArray(5 - rating)].map((_, index) => (
+        <FaStar key={index + rating} style={{ color: "#331c41" }} />
+      ))}
+      <h1>{rating}</h1>
+    </div>
+  );
+}
