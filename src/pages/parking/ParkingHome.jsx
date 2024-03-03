81290a199779a786865eec97639a917897aab064
Esraa Fathy <esraafa2411@gmail.com>
2024-03-03 06:19:28 -0800

    all

--- src/pages/parking/ParkingHome.jsx
+++ src/pages/parking/ParkingHome.jsx
@@ -3,21 +3,24 @@ import ParkingCard from "../../components/dashboader/ParkingCard";
 import ParkingFilter from "../../components/dashboader/ParkingFilter";
 import { useState } from "react";
 export default function ParkingHome() {
-  const [userParkings, setUserParkings] = useState([])
+  const [userParkings, setUserParkings] = useState([]);
   return (
-    <div  className={`d-flex flex-column m-md-5 m-2`}>
+    <div className={`d-flex flex-column m-md-5 m-2`}>
       <div className={`d-md-flex w-md-75 mb-5 pb-md-4 pb-1 border-bottom`}>
         <div className="mb-md-0 mb-3 col-6 col-md-5">
-          <Link to={`/Profile/parking`} className="text-decoration-none btn btn-outline-warning text-dark">اضافة موقف جديد</Link>
+          <Link to={`/Profile/parking`} className="text-decoration-none btn btn-outline-warning text-dark">
+            اضافة موقف جديد
+          </Link>
         </div>
         <div className={`d-flex w-75`}>
-        <ParkingFilter value={"approved"} text="نشط" setUserParkings={setUserParkings}/>
-        <ParkingFilter value={"pending"} text="قيد الانتظار" setUserParkings={setUserParkings}/>
-        <ParkingFilter value={"rejected"} text="مرفوض" setUserParkings={setUserParkings}/>
+          <ParkingFilter value={""} text="الجميع" setUserParkings={setUserParkings} />
+          <ParkingFilter value={"?status=approved"} text="نشط" setUserParkings={setUserParkings} />
+          <ParkingFilter value={"?status=pending"} text="قيد الانتظار" setUserParkings={setUserParkings} />
+          <ParkingFilter value={"?status=rejected"} text="مرفوض" setUserParkings={setUserParkings} />
         </div>
       </div>
       <div className={`align-self-center w-75`}>
-        <ParkingCard  userParkings={userParkings} setUserParkings={setUserParkings}/>
+        <ParkingCard userParkings={userParkings} setUserParkings={setUserParkings} />
       </div>
     </div>
   );
