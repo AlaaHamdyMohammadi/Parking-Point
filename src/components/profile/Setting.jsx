eba13806409f42eca1703988decf3745f839b929
marim27 <marimmohmed@outlook.com>
2024-03-14 13:44:58 +0200

    Change Password

---
+++ src/components/profile/Setting.jsx
@@ -0,0 +1,173 @@
+import { useSelector } from "react-redux";
+import useLogInUserData from "../../../hook/useLogInUserData";
+import { useState } from "react";
+import axiosInstanceParking from "../../axiosConfig/instanc";
+import NameInputs from "../formFun/nameInputs";
+import PhoneInput from "../formFun/PhoneInput";
+import EmailInput from "../formFun/EmailInput";
+import CarTypeInput from "../formFun/CarTypeInput";
+import classes from "./../../styles/formStyles.module.css";
+import RegionInput from "../formFun/RegionInput";
+import CitySelect from "../formFun/CitySelect";
+import StateInput from "../formFun/StateInput";
+import PlateNumberInput from "../formFun/PlateNumberInput";
+export default function Setting() {
+    const token = useSelector((state) => state.loggedIn.token);
+    const user = useLogInUserData();
+
+    const [userInfo, setUserInfo] = useState({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        city: user.city,
+        state: user.state,
+        region: user.region,
+        plateNumber: user.plateNumber,
+        carType: user.carType,
+    });
+    const [errors, setErrors] = useState({
+        fristNameErrors: "",
+        lastNameErrors: "",
+        emailErrors: "",
+        phoneNumberErrors: "",
+        cityErrors: "",
+        stateErrors: "",
+        nationaleIdErrors: "",
+        regionErrors: "",
+        plateNumberErrors: "",
+        carTypeErrors: "",
+    });
+    const handleSubmit = async (event) => {
+        const hasErrors = Object.values(errors).some((error) => error !== "");
+        const isEmpty = Object.values(userInfo).some((userInf) => userInf === "");
+        if (hasErrors || isEmpty) {
+            event.preventDefault();
+        } else {
+            event.preventDefault();
+            try {
+                const res = await axiosInstanceParking.patch(`/users/me`, userInfo, {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                const userData = res.data;
+                console.log(userData);
+            } catch (error) {
+                console.error("not login", error);
+            }
+        }
+    };
+    return (
+        <main>
+            <form method="post" onSubmit={handleSubmit}>
+                <div className="d-flex flex-column pe-5 align-self-center align-self-start w-md-75">
+                    <div className="row flex-column flex-sm-row ">
+                        <NameInputs
+                            nameInfo={userInfo}
+                            classes={classes}
+                            setNameInfo={setUserInfo}
+                            errors={errors}
+                            setErrors={setErrors}
+                        />
+                        <div className="col-md-6 col-12">
+                            <PhoneInput
+                                phoneNumberInfo={userInfo}
+                                classes={classes}
+                                setPhoneNumberInfo={setUserInfo}
+                                errors={errors}
+                                setErrors={setErrors}
+                            />
+                        </div>
+                        <div className="col-md-6 col-sm-12">
+                            <EmailInput
+                                emailInfo={userInfo}
+                                classes={classes}
+                                setEmailInfo={setUserInfo}
+                                errors={errors}
+                                setErrors={setErrors}
+                            />
+                        </div>
+                        {user.role === "driver" && (
+                            <>
+                                <div className="col-md-6 col-sm-12">
+                                    <CarTypeInput
+                                        carTypeInfo={userInfo}
+                                        classes={classes}
+                                        setCarTypeInfo={setUserInfo}
+                                        errors={errors}
+                                        setErrors={setErrors}
+                                    />
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <PlateNumberInput
+                                        plateNumberInfo={userInfo}
+                                        classes={classes}
+                                        setPlateNumberInfo={setUserInfo}
+                                        errors={errors}
+                                        setErrors={setErrors}
+                                    />
+                                </div>
+                            </>
+                        )}
+                        {user.role == "renter" && (
+                            <>
+                                <div className="col-md-6 col-12">
+                                    <CitySelect
+                                        cityInfo={userInfo}
+                                        classes={classes}
+                                        setCityInfo={setUserInfo}
+                                        errors={errors}
+                                        setErrors={setErrors}
+                                    />
+                                </div>
+                                <div className="col-md-6 col-12">
+                                    <RegionInput
+                                        regionInfo={userInfo}
+                                        classes={classes}
+                                        setRegionInfo={setUserInfo}
+                                        errors={errors}
+                                        setErrors={setErrors}
+                                    />
+                                </div>
+                                <div className="col-md-6 col-12">
+                                    <StateInput
+                                        stateInfo={userInfo}
+                                        classes={classes}
+                                        setStateInfo={setUserInfo}
+                                        errors={errors}
+                                        setErrors={setErrors}
+                                    />
+                                </div>
+                                <div className={`col-md-6 col-12`}>
+                                    <label className="fs-5" htmlFor="nationaleId">
+                                        رقم الهوية
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="nationaleId"
+                                        id="nationaleId"
+                                        value={user.nationaleId}
+                                        disabled
+                                        className={`${classes.input} form-control border border-secondary shadow-none`}
+                                    />
+                                    <p className={`${classes.error} text-danger`}>{errors.nationaleIdErrors}</p>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                    <div className="row d-flex justify-content-center">
+                        <input
+                            type="submit"
+                            value="تحديث"
+                            className={
+                                Object.values(errors).some((error) => error !== "")
+                                    ? `text-center bgColor w-50 text-white btn my-3 ${classes.formBtn} disabled`
+                                    : `text-center bgColor w-50 text-white btn my-3 ${classes.formBtn}`
+                            }
+                            disabled={Object.values(userInfo).some((userInfo) => userInfo == "")}
+                        />
+                    </div>
+                </div>
+            </form>
+        </main>
+    )
+}
--- src/pages/parking/EditProfile.jsx
+++ src/pages/parking/EditProfile.jsx
@@ -1,175 +1,27 @@
-import { useState } from "react";
 import Photoprofile from "./../../components/profile/photoprofile";
-import classes from "./../../styles/formStyles.module.css";
-import useLogInUserData from "../../../hook/useLogInUserData";
-import CitySelect from "../../components/formFun/CitySelect";
-import StateInput from "../../components/formFun/StateInput";
-import RegionInput from "../../components/formFun/RegionInput";
-import EmailInput from "../../components/formFun/EmailInput";
-import PhoneInput from "../../components/formFun/PhoneInput";
-import NameInputs from "../../components/formFun/nameInputs";
-import PlateNumberInput from "../../components/formFun/PlateNumberInput";
-import CarTypeInput from "../../components/formFun/CarTypeInput";
-import axiosInstanceParking from "../../axiosConfig/instanc";
-import { useSelector } from "react-redux";
+import Setting from "../../components/profile/Setting";
+import ChangePassword from "../../components/profile/ChangePassword";
+import { useState } from "react";
 
 export default function EditProfile() {
-  const token = useSelector((state) => state.loggedIn.token);
-  const user = useLogInUserData();
-  const [userInfo, setUserInfo] = useState({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    phoneNumber: user.phoneNumber,
-    city: user.city,
-    state: user.state,
-    region: user.region,
-    plateNumber: user.plateNumber,
-    carType: user.carType,
-  });
-  const [errors, setErrors] = useState({
-    fristNameErrors: "",
-    lastNameErrors: "",
-    emailErrors: "",
-    phoneNumberErrors: "",
-    cityErrors: "",
-    stateErrors: "",
-    nationaleIdErrors: "",
-    regionErrors: "",
-    plateNumberErrors: "",
-    carTypeErrors: "",
-  });
-  const handleSubmit = async (event) => {
-    const hasErrors = Object.values(errors).some((error) => error !== "");
-    const isEmpty = Object.values(userInfo).some((userInf) => userInf === "");
-    if (hasErrors || isEmpty) {
-      event.preventDefault();
-    } else {
-      event.preventDefault();
-      try {
-        const res = await axiosInstanceParking.patch(`/users/me`, userInfo, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        const userData = res.data;
-        console.log(userData);
-      } catch (error) {
-        console.error("not login", error);
-      }
-    }
-  };
+  const [show, setShow] = useState(true);
+  function setting() {
+    setShow(true);
+  }
+  function changePassword() {
+    setShow(false);
+  }
   return (
     <>
       <Photoprofile />
-      <form method="post" onSubmit={handleSubmit}>
-        <div className="d-flex flex-column mt-5 pe-5 align-self-center gap-6 align-self-start w-75">
-          <div className="row flex-column flex-sm-row ">
-            <NameInputs
-              nameInfo={userInfo}
-              classes={classes}
-              setNameInfo={setUserInfo}
-              errors={errors}
-              setErrors={setErrors}
-            />
-            <div className="col-md-6 col-12">
-              <PhoneInput
-                phoneNumberInfo={userInfo}
-                classes={classes}
-                setPhoneNumberInfo={setUserInfo}
-                errors={errors}
-                setErrors={setErrors}
-              />
-            </div>
-            <div className="col-md-6 col-sm-12">
-              <EmailInput
-                emailInfo={userInfo}
-                classes={classes}
-                setEmailInfo={setUserInfo}
-                errors={errors}
-                setErrors={setErrors}
-              />
-            </div>
-            {user.role === "driver" && (
-              <>
-                <div className="col-md-6 col-sm-12">
-                  <CarTypeInput
-                    carTypeInfo={userInfo}
-                    classes={classes}
-                    setCarTypeInfo={setUserInfo}
-                    errors={errors}
-                    setErrors={setErrors}
-                  />
-                </div>
-                <div className="col-md-6 col-sm-12">
-                  <PlateNumberInput
-                    plateNumberInfo={userInfo}
-                    classes={classes}
-                    setPlateNumberInfo={setUserInfo}
-                    errors={errors}
-                    setErrors={setErrors}
-                  />
-                </div>
-              </>
-            )}
-            {user.role == "renter" && (
-              <>
-                <div className="col-md-6 col-12">
-                  <CitySelect
-                    cityInfo={userInfo}
-                    classes={classes}
-                    setCityInfo={setUserInfo}
-                    errors={errors}
-                    setErrors={setErrors}
-                  />
-                </div>
-                <div className="col-md-6 col-12">
-                  <RegionInput
-                    regionInfo={userInfo}
-                    classes={classes}
-                    setRegionInfo={setUserInfo}
-                    errors={errors}
-                    setErrors={setErrors}
-                  />
-                </div>
-                <div className="col-md-6 col-12">
-                  <StateInput
-                    stateInfo={userInfo}
-                    classes={classes}
-                    setStateInfo={setUserInfo}
-                    errors={errors}
-                    setErrors={setErrors}
-                  />
-                </div>
-                <div className={`col-md-6 col-12`}>
-                  <label className="fs-5" htmlFor="nationaleId">
-                    رقم الهوية
-                  </label>
-                  <input
-                    type="text"
-                    name="nationaleId"
-                    id="nationaleId"
-                    value={user.nationaleId}
-                    disabled
-                    className={`${classes.input} form-control border border-secondary shadow-none`}
-                  />
-                  <p className={`${classes.error} text-danger`}>{errors.nationaleIdErrors}</p>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="row d-flex justify-content-center">
-            <input
-              type="submit"
-              value="تحديث"
-              className={
-                Object.values(errors).some((error) => error !== "")
-                  ? `text-center bgColor w-50 text-white btn my-3 ${classes.formBtn} disabled`
-                  : `text-center bgColor w-50 text-white btn my-3 ${classes.formBtn}`
-              }
-              disabled={Object.values(userInfo).some((userInfo) => userInfo == "")}
-            />
-          </div>
-        </div>
-      </form>
+      <div className="mt-5 pe-5">
+        <button className="btn btn-outline-warning text-dark m-2" onClick={setting}>تعديل البيانات</button>
+        <button className="btn btn-outline-warning text-dark" onClick={changePassword}>تغيير كلمة السر</button>
+      </div>
+      {show ?
+        <Setting /> :
+        <ChangePassword />
+      }
     </>
   );
 }
