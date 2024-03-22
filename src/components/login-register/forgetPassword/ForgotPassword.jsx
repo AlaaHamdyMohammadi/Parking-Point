21bf156f30aaa60008189f76859f94592772012c
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2024-03-22 22:45:56 +0200

    Update forget password

--- src/components/login-register/forgetPassword/ForgotPassword.jsx
+++ src/components/login-register/forgetPassword/ForgotPassword.jsx
@@ -85,7 +85,7 @@ const ForgotPassword = () => {
 
   async function handleForgotPassword() {
     try {
-      const res = await axiosInstanceParking.post("/users/me/forgetPassword", {
+      const res = await axiosInstanceParking.post("/users/me/forget-password", {
         email,
       });
       console.log(res.data);
@@ -96,7 +96,7 @@ const ForgotPassword = () => {
 
   async function handleResetPassword() {
     try {
-      const res = await axiosInstanceParking.post("/users/me/resetPassword", {
+      const res = await axiosInstanceParking.post("/users/me/reset-password", {
         token,
         email,
         password,
@@ -146,7 +146,10 @@ const ForgotPassword = () => {
                     className={`${classes.input}  w-100 mt-2 form-control border-secondary shadow-none`}
                     /*value={registeUser.email}*/
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                      console.log("Email:", e.target.value);
+                    }}
                   />
                 </div>
                 <div className="text-start align-self-center">
@@ -205,7 +208,10 @@ const ForgotPassword = () => {
                     <input
                       type="text"
                       value={token}
-                      onChange={(e) => setToken(e.target.value)}
+                      onChange={(e) => {
+                        setToken(e.target.value);
+                        console.log("Token:", e.target.value);
+                      }}
                       className={`${classes.input}  w-100 mt-2 form-control border-secondary shadow-none`}
                     />
                     {/* <ConfirmationCodeInput length={6} onConfirm={(code) => console.log("Confirmed:", code)} /> */}
@@ -268,12 +274,17 @@ const ForgotPassword = () => {
                             type={showPassword ? "text" : "password"}
                             name="password"
                             id="password"
+                            value={password}
+                            onChange={(e) => {
+                              registeValidation;
+                              setPassword(e.target.value);
+                              console.log("Password:", e.target.value);
+                            }}
                             className={`${classes.input} form-control border-secondary shadow-none`}
                             style={{
                               borderTopRightRadius: "0.375rem",
                               borderBottomRightRadius: "0.375rem",
                             }}
-                            onChange={registeValidation}
                             onBlur={registeValidation}
                           />
                           <button
@@ -303,12 +314,17 @@ const ForgotPassword = () => {
                             type={showPassword ? "text" : "password"}
                             name="confirmPassword"
                             id="confirmPassword"
+                            value={confirmPassword}
+                            onChange={(e) => {
+                              registeValidation;
+                              setConfirmPassword(e.target.value);
+                              console.log("ConfirmPassword:", e.target.value);
+                            }}
                             className={`${classes.input} form-control border-secondary shadow-none`}
                             style={{
                               borderTopRightRadius: "0.375rem",
                               borderBottomRightRadius: "0.375rem",
                             }}
-                            onChange={registeValidation}
                             onBlur={registeValidation}
                           />
                           <button
