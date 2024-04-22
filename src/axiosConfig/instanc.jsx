46a708b7ec742a99518dbd372eceadf0b310fd5d
marim27 <marimmohmed@outlook.com>
2024-04-23 01:42:09 +0200

    Update instanc.jsx

--- src/axiosConfig/instanc.jsx
+++ src/axiosConfig/instanc.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstanceParking= axios.create({
-baseURL:'http://localhost:3000',
+baseURL:'https://e-parking-backend.onrender.com',
 })
 
 axiosInstanceParking.interceptors.request.use((req)=>{
