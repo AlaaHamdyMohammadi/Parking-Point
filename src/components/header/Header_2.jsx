89ce22eb8f4c3697caf9c21160d104d329a42029
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-04-23 02:34:14 +0200

    push

--- src/components/header/Header_2.jsx
+++ src/components/header/Header_2.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
-import classes from "./../../styles/Header_2.module.css";
+import classes from "./../../styles/header_2.module";
 
 export default function Header_2() {
   return (
-    <nav className={`shadow ${classes.heightCustom} d-flex   justify-content-end`}>
-      {/* <div className=" flex-grow"></div> */}
+    <nav
+      className={`shadow ${classes.heightCustom} d-flex   justify-content-end`}
+    >
       <div className="">
         <Link to={`/`}>
-          <img src="/parkingpointlogo2.png" className={`${classes.logo} mt-1  p-1 mx-4`} />
+          <img
+            src="/parkingpointlogo2.png"
+            className={`${classes.logo} mt-1  p-1 mx-4`}
+          />
         </Link>
       </div>
     </nav>
