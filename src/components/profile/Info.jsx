84eeef316571b51ddc1cfef7cdba32d794fcf196
marim27 <marimmohmed@outlook.com>
2024-04-26 21:45:38 +0300

    Update Info.jsx

--- src/components/profile/Info.jsx
+++ src/components/profile/Info.jsx
@@ -2,7 +2,7 @@
 export default function Info({ lable, info, icon }) {
   return (
     <>
-      <div className="card  m-4 slide-in-right animate shadow border-0 ">
+      <div className="card my-4 slide-in-right animate shadow border-0 ">
         <p className=" fw-bold p-2">
           <span className="p-2 ">{icon}</span>
           <span>{lable}</span>
