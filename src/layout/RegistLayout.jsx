368798065cd05dec38ef15b6639c3cb6405d8555
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-04-27 22:58:34 +0300

    min h

--- src/layout/RegistLayout.jsx
+++ src/layout/RegistLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function RegistLayout() {
   return (
-    <div>
+    <div style={{ minHeight: "35.5vw" }}>
       <Outlet />
     </div>
   );
