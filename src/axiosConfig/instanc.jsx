01311b5bc53a4e176e08cdff8ec77c3b0d71b045
marim27 <marimmohmed@outlook.com>
2024-04-29 20:50:37 +0300

    axios

--- src/axiosConfig/instanc.jsx
+++ src/axiosConfig/instanc.jsx
@@ -2,6 +2,8 @@ import axios from "axios"
 const axiosInstanceParking= axios.create({
 baseURL:'https://e-parking-backend.onrender.com',
 withCredentials: true,
+headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+credentials: 'include',
 })
 
 axiosInstanceParking.interceptors.request.use((req)=>{
