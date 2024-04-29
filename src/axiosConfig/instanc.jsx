d7cabba4b4b68b603483720e5c37e7cc994c31cc
marim27 <marimmohmed@outlook.com>
2024-04-29 20:26:10 +0300

    Update instanc.jsx

--- src/axiosConfig/instanc.jsx
+++ src/axiosConfig/instanc.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 const axiosInstanceParking= axios.create({
 baseURL:'https://e-parking-backend.onrender.com',
-withCredentials: false,
+withCredentials: true,
 })
 
 axiosInstanceParking.interceptors.request.use((req)=>{
