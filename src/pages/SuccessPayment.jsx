d710927fe48eb6b15eba5dba8615a0d938b3e41c
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-05-08 01:51:34 +0300

    handle error payments

--- src/pages/SuccessPayment.jsx
+++ src/pages/SuccessPayment.jsx
@@ -28,6 +28,9 @@ const SuccessPayment = () => {
         const price = ReserveResponse.data.reserve.price;
 
         setParkId(newParkId);
+
+        toast.success(t("successPaymentSuccess"));
+
         setTimeout(() => {
           navigate(
             `/ParkDetials?from=${From}&to=${To}&price=${price}&newParkId=${newParkId}`
@@ -35,10 +38,17 @@ const SuccessPayment = () => {
         }, 2000);
       } catch (err) {
         toast.error(t("successPaymentError"));
-        console.error("Error :", err);
-        if (err.response) {
-          console.error("Response data:", err.response.data);
-        }
+        setTimeout(() => {
+          navigate(`/`);
+        }, 2000);
+        // console.error("Error :", err);
+        // if (err.response) {
+        //   toast.error(t("successPaymentError"));
+        //   setTimeout(() => {
+        //     navigate(`/`);
+        //   }, 2000);
+        //   console.error("Response data:", err.response.data);
+        // }
       }
     };
 
@@ -55,7 +65,9 @@ const SuccessPayment = () => {
           <div className="border border-3 border-success"></div>
           <div className="card  bg-white shadow p-5">
             <div className="mb-4 text-center mx-auto">
-              <svg
+              <Spinner animation="border" variant="success" size="lg" />
+
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="75"
                 height="75"
@@ -66,7 +78,7 @@ const SuccessPayment = () => {
               >
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                 <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
-              </svg>
+              </svg> */}
             </div>
             <div className="text-center ">
               <h2>{t("successPayment1")}</h2>
