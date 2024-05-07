50f15cef89a9989f663571a6e1f0dd493a22712c
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-05-08 01:56:19 +0300

    Update SuccessPayment.jsx

--- src/pages/SuccessPayment.jsx
+++ src/pages/SuccessPayment.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
@@ -10,6 +11,8 @@ import "react-toastify/dist/ReactToastify.css";
 const SuccessPayment = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const [paymentsuccess, setPaymentsuccess] = useState(false);
+
   const [parkId, setParkId] = useState("");
   useEffect(() => {
     const handlePayment = async () => {
@@ -28,7 +31,7 @@ const SuccessPayment = () => {
         const price = ReserveResponse.data.reserve.price;
 
         setParkId(newParkId);
-
+        setPaymentsuccess(true);
         toast.success(t("successPaymentSuccess"));
 
         setTimeout(() => {
@@ -53,7 +56,7 @@ const SuccessPayment = () => {
     };
 
     handlePayment();
-  }, [navigate]);
+  }, [navigate, paymentsuccess]);
 
   return (
     <>
@@ -65,20 +68,22 @@ const SuccessPayment = () => {
           <div className="border border-3 border-success"></div>
           <div className="card  bg-white shadow p-5">
             <div className="mb-4 text-center mx-auto">
-              <Spinner animation="border" variant="success" size="lg" />
-
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="75"
-                height="75"
-                color="#198754"
-                fill="currentColor"
-                className=" text-success bi bi-check-circle"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
-              </svg> */}
+              {!paymentsuccess ? (
+                <Spinner animation="border" variant="success" size="lg" />
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="75"
+                  height="75"
+                  color="#198754"
+                  fill="currentColor"
+                  className=" text-success bi bi-check-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                </svg>
+              )}
             </div>
             <div className="text-center ">
               <h2>{t("successPayment1")}</h2>
