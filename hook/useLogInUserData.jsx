205714aa677333947193aee06c6228eabd47f89a
marim27 <marimmohmed@outlook.com>
2024-05-08 17:05:22 +0300

    Update useLogInUserData.jsx

--- hook/useLogInUserData.jsx
+++ hook/useLogInUserData.jsx
@@ -24,6 +24,6 @@ export default function useLogInUserData() {
             }
         };
         getLogInUser()
-    }, [dispatch,navigate,user]);
+    }, [dispatch,navigate]);
     return user;
 }
