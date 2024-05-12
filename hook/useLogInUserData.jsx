cfbb04c7e8a86329f1ba5a567df01e9fe6fc79b6
marim27 <marimmohmed@outlook.com>
2024-05-12 15:09:14 +0300

    Update useLogInUserData.jsx

--- hook/useLogInUserData.jsx
+++ hook/useLogInUserData.jsx
@@ -3,31 +3,40 @@ import axiosInstanceParking from "../src/axiosConfig/instanc";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logout } from "../src/store/slices/authSlice";
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 export default function useLogInUserData() {
     const token = Cookies.get('token');
-    const [user, setuser] = useState({});
+    console.log(token);
+    const [user, setuser] = useState({})
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const logOut = async () => {
-        try {
-            await axiosInstanceParking.post(`/users/logout`);
-            dispatch(logout());
-            navigate("/");
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    // useEffect(() => {
+    //     const getLogInUser = async () => {
+    //         try {
+    //             const response = await axiosInstanceParking.get('/users/me'
+    //             );
+    //             setuser(response.data.doc);
+    //             return response.data.doc;
+    //         } catch (error) {
+    //             if (error.response.data.error == 'the user isnt exist anymore' || error.response.status==401) {
+    //                 dispatch(logout());
+    //                 await axiosInstanceParking.post(`/users/logout`);
+    //                 navigate("/");
+    //             }
+    //         }
+    //     };
+    //     getLogInUser()
+    // }, [dispatch,navigate]);
     useEffect(() => {
         const getLogInUser = async () => {
             try {
                 const response = await axiosInstanceParking.get("/users/me");
                 setuser(response.data.doc);
-                return response.data.doc;
             } catch (error) {
                 if (
-                    error.response.data.error == "the user isnt exist anymore" ||
-                    error.response.status == 401
+                    error.response &&
+                    (error.response.data.error === "the user isnt exist anymore" ||
+                    error.response.status === 401)
                 ) {
                     dispatch(logout());
                     await axiosInstanceParking.post(`/users/logout`);
@@ -35,14 +44,16 @@ export default function useLogInUserData() {
                 }
             }
         };
-        getLogInUser();
-    }, [dispatch, navigate]);
-    useEffect(() => {
-        console.log(token);
-        if (!token) {
-            console.log("notToken");
-            // logOut()
+
+        if (token) {
+            getLogInUser();
+            console.log(token);
+        } else {
+            // If there's no token, logout the user
+            dispatch(logout());
+            navigate("/");
+            console.log('token');
         }
-    }, []);
+    }, [dispatch, navigate, token]);
     return user;
 }
